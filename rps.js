cf214ff0da4e7cd41de336432b9d879d3ec87633
Coleman Ellis <colemancellis@gmail.com>
2023-08-31 17:00:19 -0400

    Add game win logic

--- rps.js
+++ rps.js
@@ -84,10 +84,12 @@ function clickedChoice(e) {
     switch (result) {
         case winner.player: {
             resultemoji = "✅";
+            numplayerwins += 1; // Sneak this into the emoji selection
             break;
         }
         case winner.computer: {
             resultemoji = "❌";
+            numcomputerwins += 1;
             break;
         }
         case winner.tie: {
@@ -96,10 +98,29 @@ function clickedChoice(e) {
         }
     }
 
+    // A row in the matches table showing each choice and the result
     let div_result = document.createElement("div");
     div_result.textContent = `${playeremoji}${computeremoji}${resultemoji}`;
     matches.appendChild(div_result);
+
+    // Best of 5, so first to 3 wins
+    if (numplayerwins === 3) {
+        let div_win = document.createElement("div");
+        div_win.textContent = "🏆🧑‍💻🏆";
+        matches.appendChild(div_win);
+        const options = document.querySelectorAll("div.option");
+        options.forEach(option => option.removeEventListener("click", clickedChoice));
+    }
+    if (numcomputerwins === 3) {
+        let div_loss = document.createElement("div");
+        div_loss.textContent = "🏆🤖🏆";
+        matches.appendChild(div_loss);
+        const options = document.querySelectorAll("div.option");
+        options.forEach(option => option.removeEventListener("click", clickedChoice));
+    }
 }
 
+let numplayerwins = 0;
+let numcomputerwins = 0;
 const options = document.querySelectorAll("div.option");
 options.forEach(option => option.addEventListener("click", clickedChoice));
